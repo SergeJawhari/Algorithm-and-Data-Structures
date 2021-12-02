0631bf2dbac6c50978cc5d893ac2c0e226997aab
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-12-01 23:03:12 -0800

    Added comments and summary
    added comments to my personal solution code as well as created a summary readme file

--- Tournament Winner/o_of_n_time_my_solution.py
+++ Tournament Winner/o_of_n_time_my_solution.py
@@ -3,31 +3,31 @@
 
 # O(n) time complexity where n is the length of the array | O(n) space complexity because the dictionary must be added to and updated.
 
-competitions = [
-    ["HTML", "C#"],
-    ["C#", "Python"],
-    ["Python", "HTML"]
-]
-
-results = [0,0,1]
-
 def tournamentWinner(competitions, results):
 
-    resultsIndex = 0
+    resultsIndex = 0 # initialized the index of the results to 0.
 
-    winnersPoints = {}
+    winnersPoints = {} # created an empty dictionary.
 
-    for i in results:
-        if i == 0:
+    for result in results:
+
+        # this if statement will check if the value of each result to see if it is the away team that has won.
+        if result == 0:
+
+            # if the key or name of team has won, then the key value will be updated with a +3 points for winning.
             if (competitions[resultsIndex][1]) in winnersPoints:
                 winnersPoints[(competitions[resultsIndex][1])] += 3
+
+            # else, the key value pair will be initialized with a win, denoted by the 3 initial points for winning.
             else:
                 winnersPoints[(competitions[resultsIndex][1])] = 3
 
+            # if the losing team is not in the dictionary, then it will be added with the 0 points for losing.
             if (competitions[resultsIndex][0]) not in winnersPoints:
                 winnersPoints[(competitions[resultsIndex][0])] = 0
 
-        elif i == 1:
+        # this statement works in vice verse of the if statement above, instead checking if the home team has won.
+        elif result == 1:
             if (competitions[resultsIndex][0]) in winnersPoints:
                 winnersPoints[(competitions[resultsIndex][0])] += 3
             else:
@@ -36,11 +36,8 @@ def tournamentWinner(competitions, results):
             if (competitions[resultsIndex][1]) not in winnersPoints:
                 winnersPoints[(competitions[resultsIndex][1])] = 0
         
-        resultsIndex += 1
+        resultsIndex += 1 # this will increment the result index by 1 point.
     
-    winner = max(winnersPoints, key = winnersPoints.get)
+    winner = max(winnersPoints, key = winnersPoints.get) # the winner of the competition will be the key of the value that has the greatest number value.
     
-    return winner
-
-
-tournamentWinner(competitions, results)
+    return winner # the string key value will be returned.
